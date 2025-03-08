316901064f6b0c6c568f5b2e20bf70f9de02bc2c
KushanTharaka <kushan.tharaka@axiatadigitallabs.com>
2025-03-08 21:32:04 +0530

    Updated the services and repository service methods.

--- src/main/java/com/ecom/fullstack/ecommerce/repositories/CategoryRepository.java
+++ src/main/java/com/ecom/fullstack/ecommerce/repositories/CategoryRepository.java
@@ -1,4 +1,13 @@
 package com.ecom.fullstack.ecommerce.repositories;
 
-public interface CategoryRepository {
+import com.ecom.fullstack.ecommerce.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+
+
+
 }
--- src/main/java/com/ecom/fullstack/ecommerce/services/CategoryService.java
+++ src/main/java/com/ecom/fullstack/ecommerce/services/CategoryService.java
@@ -1,4 +1,32 @@
 package com.ecom.fullstack.ecommerce.services;
 
+import com.ecom.fullstack.ecommerce.model.Category;
+import com.ecom.fullstack.ecommerce.model.Product;
+import com.ecom.fullstack.ecommerce.repositories.CategoryRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
 public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> getAllTheProducts(){
+        log.info("started the get all products service");
+        return categoryRepository.findAll();
+    }
+
+
+    public Category getCategoryById(Long categoryIdToSearch){
+        log.info("started the get all products service");
+        return categoryRepository.findById(categoryIdToSearch).get();
+    }
+
 }
+
